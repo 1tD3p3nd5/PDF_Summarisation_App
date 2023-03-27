8668700c7c0766d708404ad2af791b37828fd39c
Guy Geron <109926998+1tD3p3nd5@users.noreply.github.com>
2023-03-28 09:29:00 +1100

    Update run.py

--- run.py
+++ run.py
@@ -4,24 +4,4 @@ from app import flask_app
 if __name__ == "__main__":
     flask_app.run(debug=True, port=5001)
 
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_default_secret_key'
-    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'app', 'uploads')
-    ALLOWED_EXTENSIONS = {'pdf'}
-
-
-from app import flask_app
-
-if __name__ == "__main__":
-    flask_app.run(debug=True, port=5001)
-
-
-
-from app import flask_app
-
-if __name__ == "__main__":
-    flask_app.run(debug=True, port=5001)
-
 
